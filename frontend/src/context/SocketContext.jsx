a9d29774d632d802cfb8fd13e0006db32a838550
Mohit Lakhara <mohitlakhara78@gmail.com>
2026-02-26 21:29:18 +0530

    fix: set explicit production socket io url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io({
+			const socket = io("https://syncr-chat.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
